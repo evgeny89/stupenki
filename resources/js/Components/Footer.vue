e3be5c096fbf0fb7ce7937de2eabff8cbf9ebeee
lindoro7 <54413736+lindoro7@users.noreply.github.com>
2021-05-28 00:26:16 +0300

    Натяжка главной страницы

---
+++ resources/js/Components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer class="footer_grey">
+        <div class="wrapper footer_box">
+            <h4>© 2021 Stupenki.fun All Rights Reserved.</h4>
+            <div class="social">
+                <a class="social_f" href=""><i class="fab fa-facebook-f"></i></a>
+                <a class="social_f" href=""><i class="fab fa-twitter"></i></a>
+                <a class="social_f" href=""><i class="fab fa-linkedin-in"></i></a>
+                <a class="social_f" href=""><i class="fab fa-pinterest-p"></i></a>
+                <a class="social_f" href=""><i class="fab fa-google-plus-g"></i></a>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
---
+++ resources/js/Components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <header class="header">
+            <div class="wrapper menu">
+                <div></div>
+                <nav class="nav">
+                    <a class="menu_nav" href="#">О Проекте</a>
+                    <a class="menu_nav" href="#">Поиск</a>
+                    <a class="menu_nav" href="#">Регистрация</a>
+                </nav>
+                <a href="#" class="btn-login">Вход</a>
+            </div>
+        </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
