984481efd920e0dcb55d9e8bf7fa37cbed616b12
Evgeny <puls2007@rambler.ru>
2021-06-05 00:25:32 +0500

    Миграции нового функционала.

---
+++ database/migrations/2021_06_04_092837_alter_stupenki_table_add_city_and_country_and_image_columns.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterStupenkiTableAddCityAndCountryAndImageColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable('stupenki') && !Schema::hasColumns('stupenki', ['country', 'city', 'image'])) {
+            Schema::table('stupenki', function (Blueprint $table) {
+                $table->string('city')->nullable();
+                $table->string('country')->nullable();
+                $table->string('image')->nullable();
+                $table->string('name')->nullable();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasTable('stupenki') && Schema::hasColumns('stupenki', ['country', 'city', 'image'])) {
+            Schema::table('stupenki', function (Blueprint $table) {
+                $table->dropColumn('country');
+                $table->dropColumn('city');
+                $table->dropColumn('image');
+            });
+        }
+    }
+}
