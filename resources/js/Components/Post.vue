88c9e73da930a1d1295ed51d06c63825de73c42b
Evgeny <puls2007@rambler.ru>
2021-06-11 00:01:55 +0500

    Обернуть карточку в роут

--- resources/js/Components/Post.vue
+++ resources/js/Components/Post.vue
@@ -1,21 +1,21 @@
 <template>
-    <div class="user-post">
-        <h3 class="post-title">{{ post.name }}</h3>
-        <p>страна: {{ post.country.name }}, город: {{ post.city.name }}</p>
-        <p>количество ступенек: {{ post.count }}</p>
-        <p>добавлено: {{ post['created_at'] }}г.</p>
-        <div class="image-block">
-            <img :src="post['image_small']"
-                 :alt="post.name"
-                 class="image-small-size"
-            >
-            <img :src="post['image_origin']"
-                 :alt="post.name + 'origin'"
-                 class="image-full-size"
-            >
-        </div>
-        <p v-if="$root.userToken" class="post-comments_link">Комментарии ({{ post['comments_count'] }})</p>
-    </div>
+    <router-link :to="{ name: 'post', params: { id: post.id } }">
+            <h3 class="post-title">{{ post.name }}</h3>
+            <p>страна: {{ post.country.name }}, город: {{ post.city.name }}</p>
+            <p>количество ступенек: {{ post.count }}</p>
+            <p>добавлено: {{ post['created_at'] }}г.</p>
+            <div class="image-block">
+                <img :src="post['image_small']"
+                     :alt="post.name"
+                     class="image-small-size"
+                >
+                <img :src="post['image_origin']"
+                     :alt="post.name + 'origin'"
+                     class="image-full-size"
+                >
+            </div>
+            <p v-if="$root.userToken" class="post-comments_link">Комментариев: {{ post['comments_count'] }}</p>
+    </router-link>
 </template>
 
 <script>
