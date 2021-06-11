7947695fcfb2ec89c12de365f49a33bba34acc73
Evgeny <puls2007@rambler.ru>
2021-06-11 20:47:39 +0500

    Компоненты для переиспользования

---
+++ resources/js/Components/Comment.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="comment">
+        <div class="comment-title"><p>{{ comment.user.name }}</p><p>{{ comment['created_at'] }}г.</p></div>
+        <p class="comment-text">{{ comment.text }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Comment",
+    props: ['comment']
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Components/Form.vue
@@ -0,0 +1,23 @@
+<template>
+    <form>
+        <slot name="body"></slot>
+        <button type="button" @click="showText" class="btn">
+            <slot>Отправить</slot>
+        </button>
+    </form>
+</template>
+
+<script>
+export default {
+    name: "Form",
+    methods: {
+        showText: function () {
+            this.$emit('saveComment');
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
